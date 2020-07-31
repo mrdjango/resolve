7d08155c65c12ab1317664cf06c87df9bb15cc7c
Mr Django <rishalbabu.mp@outlook.com>
2020-07-31 17:33:36 +0530

    updated

--- users/forms.py
+++ users/forms.py
@@ -12,21 +12,6 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
-def clean_email(self):
-    # Get the email
-    email = self.cleaned_data.get('email')
-
-    # Check to see if any users already exist with this email as a username.
-    try:
-        match = User.get(email=email)
-    except User.DoesNotExist:
-        # Unable to find a user, this is fine
-        return email
-
-    # A user was found with this as a username, raise an error.
-    raise forms.ValidationError('This email address is already in use.')
-
-
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
