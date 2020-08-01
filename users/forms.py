93a3406e367becfcfb4ca31304d0d97df81da3e2
Mr Django <rishalbabu.mp@outlook.com>
2020-08-02 00:39:51 +0530

    updated

--- users/forms.py
+++ users/forms.py
@@ -5,19 +5,35 @@ from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(max_length=150)
+    # email = forms.EmailField(max_length=100)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
+
 
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['email']
+        fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        # Get the email
+        email = self.cleaned_data.get('email')
+
+        # Check to see if any users already exist with this email as a username.
+        try:
+            match = User.objects.get(email=email)
+
+        except User.DoesNotExist:
+            # Unable to find a user, this is fine
+            return email
+
+        # A user was found with this as a username, raise an error.
+        raise forms.ValidationError('This email address is already in use. Please try Different one!')
+
 
 
 class ProfileUpdateForm(forms.ModelForm):
